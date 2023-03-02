d61b8231110ed7553fb94165b49629aa4d5f472c
PietroDanieli <1pie7d@gmail.com>
2023-03-02 17:04:29 +0100

    implement Category

---
+++ src/main/java/album/model/Category.java
@@ -0,0 +1,50 @@
+package album.model;
+
+import java.util.List;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToMany(mappedBy = "categories")// cascade = CascadeType.REMOVE -- questo cecchina tutte le Photo
+	@OnDelete(action = OnDeleteAction.CASCADE)//questo cancella da tutte le Photo la Category
+	private List<Photo> photos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
+}
