8dd4b3155d4b1c72983faed4057f7be8408ed791
PietroDanieli <1pie7d@gmail.com>
2023-03-02 17:20:41 +0100

    implement show Photo

--- src/main/java/album/controller/PhotoController.java
+++ src/main/java/album/controller/PhotoController.java
@@ -1,11 +1,13 @@
 package album.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import album.model.Photo;
@@ -21,6 +23,15 @@ public class PhotoController {
 		public String index(Model modIndex) {
 			List<Photo> photoList = photoRepository.findAll(); // restituisce un elenco di istanze libro
 			modIndex.addAttribute("photoList", photoList);
-			return "photos/index";
-			}
+			return "photos/index";		
+		}
+		
+		@GetMapping("/{id}")
+		public String show(@PathVariable("id") Long id, Model model) {
+
+			Optional<Photo> photo = photoRepository.findById(id); 
+			model.addAttribute("photo", photo.get());
+			return "photos/show";
+
+		}
 }
