59fe857266fcb2835afe5f30291017c09d33880f
PietroDanieli <1pie7d@gmail.com>
2023-03-03 03:16:09 +0100

    refactor lol

--- src/main/java/album/controller/PhotoController.java
+++ src/main/java/album/controller/PhotoController.java
@@ -6,32 +6,40 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import album.model.Category;
 import album.model.Photo;
+import album.repository.CategoryRepository;
 import album.repository.PhotoRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/photos")
 public class PhotoController {
 	// iniettiamo automaticamente 
+	
 		private @Autowired PhotoRepository photoRepository;
-
+		private @Autowired CategoryRepository categoryRepository;
+		
 		@GetMapping
 		public String index(Model modIndex) {
-			List<Photo> photoList = photoRepository.findAll(); // restituisce un elenco di istanze libro
+			List<Photo> photoList = photoRepository.findAll(); 
 			modIndex.addAttribute("photoList", photoList);
 			return "photos/index";		
 		}
 		
 		@GetMapping("/{id}")
 		public String show(@PathVariable("id") Long id, Model model) {
-
 			Optional<Photo> photo = photoRepository.findById(id); 
 			model.addAttribute("photo", photo.get());
 			return "photos/show";
-
 		}
+
+
 }
--- src/test/java/album/SpringIlMioFotoalbumApplicationTests.java
+++ src/test/java/album/SpringIlMioFotoalbumApplicationTests.java
@@ -1,4 +1,4 @@
-package poligonDXF;
+package album;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
