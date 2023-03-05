8eea4696e1760d92486b246e09688e8671ad2792
PietroDanieli <1pie7d@gmail.com>
2023-03-06 00:04:26 +0100

    implement method for filter

--- src/main/java/album/repository/PhotoRepository.java
+++ src/main/java/album/repository/PhotoRepository.java
@@ -1,9 +1,12 @@
 package album.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import album.model.Photo;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
-
+	public List<Photo> findByTitleContainingIgnoreCase(String title);
+	public List<Photo> findByTagContainingIgnoreCase(String tag);
 }
