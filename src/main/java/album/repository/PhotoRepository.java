15a6475f3aa8ecc91bffe26d4eff4f25c01b7b40
PietroDanieli <1pie7d@gmail.com>
2023-03-06 15:22:32 +0100

    comment test

--- src/main/java/album/repository/PhotoRepository.java
+++ src/main/java/album/repository/PhotoRepository.java
@@ -9,4 +9,5 @@ import album.model.Photo;
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 	public List<Photo> findByTitleContainingIgnoreCase(String title);
 	public List<Photo> findByTagContainingIgnoreCase(String tag);
+//	public List<Photo> findByNameContainingIgnoreCaseAndCategoryContainingIgnoreCase(String name);
 }
