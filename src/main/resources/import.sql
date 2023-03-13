2c808568a2ac4f76ba2fb89c6e79173865e925f1
PietroDanieli <1pie7d@gmail.com>
2023-03-13 16:54:14 +0100

    new element

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,19 @@
 INSERT INTO albumfotografico.photo(title, description, tag, url, visible) VALUES('Monte Bianco', "la montagna più bianca", "montagna", 'https://picsum.photos/200/150', true);
-INSERT INTO albumfotografico.photo(title, description, tag, url, visible) VALUES('Zozzomarina', 'il mare pulito', "mare", 'https://picsum.photos/200/150', false);
+INSERT INTO albumfotografico.photo(title, description, tag, url, visible) VALUES('Zozzomarina', 'il mare pulito', "mare", 'https://picsum.photos/200/150', true);
 INSERT INTO albumfotografico.photo(title, description, tag, url, visible) VALUES('New York', 'la grande mela', "metropoly", 'https://picsum.photos/200/150', true);
+INSERT INTO albumfotografico.photo(title, description, tag, url, visible) VALUES('New Ciao', 'la grande banana', "metro", 'https://picsum.photos/200/150', true);
 INSERT INTO albumfotografico.category(name) VALUES('viaggiare');
 INSERT INTO albumfotografico.category(name) VALUES('città');
 INSERT INTO albumfotografico.category(name) VALUES('moda');
 INSERT INTO albumfotografico.category(name) VALUES('tecnologia');
 INSERT INTO albumfotografico.category_photo(photo_id, category_id) VALUES(1,1);
+INSERT INTO albumfotografico.category_photo(photo_id, category_id) VALUES(4,1);
 INSERT INTO albumfotografico.category_photo(photo_id, category_id) VALUES(2,1);
 INSERT INTO albumfotografico.category_photo(photo_id, category_id) VALUES(3,2);
 INSERT INTO albumfotografico.category_photo(photo_id, category_id) VALUES(3,3);
-INSERT INTO albumfotografico.user(id, username, password) VALUES(1,'user','{bcrypt}$2a$10$4zuKJIl.sBQswhNNxSKSTO2nyLcuVRaLf4iBX4R4/s2mFzE5kCxLu');
+INSERT INTO albumfotografico.user(id, username, password) VALUES(1,'admin','{bcrypt}$2a$10$4zuKJIl.sBQswhNNxSKSTO2nyLcuVRaLf4iBX4R4/s2mFzE5kCxLu'); -- pass : miao
+INSERT INTO albumfotografico.user(id, username, password) VALUES(2,'user','{bcrypt}$2a$10$4zuKJIl.sBQswhNNxSKSTO2nyLcuVRaLf4iBX4R4/s2mFzE5kCxLu'); -- pass : miao
 INSERT INTO albumfotografico.role(id, name) VALUES(1,'ADMIN');
 INSERT INTO albumfotografico.role(id, name) VALUES(2, 'USER');
 INSERT INTO albumfotografico.user_roles(user_id, roles_id) VALUES(1,1);
+INSERT INTO albumfotografico.user_roles(user_id, roles_id) VALUES(2,2);
